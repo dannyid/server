edc821f64d0b504579a1283e32bdfdbf0f3a19a6
dannyid <danielshekhtman@gmail.com>
2013-08-15 01:14:10 -0400

    back from port 5000

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var http = require('http');
+var http = require( 'http' );
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end("Congrats! You're being listened to on port " + process.env.PORT);
+var server = http.createServer( function( req, res ) {
+    res.writeHead( 200 );
+    res.end( "Congrats! You're being listened to on port " + process.env.PORT );
 });
 
-//server.listen(process.env.PORT || 5000);
-server.listen(5000);
+server.listen( process.env.PORT || 5000 );
+//server.listen( 5000 );
