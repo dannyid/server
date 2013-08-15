b8293a8fb42716b567c6b67b996a995ce7b27652
dannyid <danielshekhtman@gmail.com>
2013-08-15 00:04:51 -0400

    UPDATING THE PORT MAN

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.end("Congrats! You're being listened to on port 80.");
+    res.end("Congrats! You're being listened to on port" + process.env.PORT);
 });
 
 server.listen(process.env.PORT || 5000);
