3cb0114a6b08ae9f7f38e2e268e6c0a672086944
dannyid <danielshekhtman@gmail.com>
2013-09-08 21:22:23 -0400

    trying to get the thing to txt me

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require( 'http' );
+var client = require('twilio')('AC5b50333372f8454519df39c0a765e50a', 'e0a1a95e3c6a8c7505a3d2677729a8bf');
 
 var server = http.createServer( function( req, res ) {
     res.writeHead( 200 );
     res.end( "Congrats! You're being listened to on port " + process.env.PORT );
 
-    var client = require('twilio')('AC5b50333372f8454519df39c0a765e50a', 'e0a1a95e3c6a8c7505a3d2677729a8bf');
 
     client.sendSms({
 
