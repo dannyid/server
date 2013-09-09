15096abb3a0500dde293512ff96742356f4fbbb8
dannyid <danielshekhtman@gmail.com>
2013-09-08 22:59:25 -0400

    adding query param 'name'

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
-var http = require( 'http' );
+var http = require('http');
+var url = require('url');
 
-var server = http.createServer( function( req, res ) {
+var server = http.createServer(function(req, res) {
+  
+    var queryData = url.parse(req.url, true).query;
     res.statusCode = 200;
 
     fs.readFile('index.html', function(err, data) {
-        if (!err) {
-        res.write(data.toString());
-        res.end();
+        if (!err && queryData.name === "Danny")  {
+            res.write(data.toString());
+            res.end();
         }
     });
+
 });
 
 server.listen( process.env.PORT || 5000 );
