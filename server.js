41e1357ddfa5dedc03805d03b94d36eca6985066
dannyid <danielshekhtman@gmail.com>
2013-09-08 21:47:07 -0400

    attempting to load html into page from node server

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var http = require( 'http' );
 
 var server = http.createServer( function( req, res ) {
     res.writeHead( 200 );
-    res.end( "Congrats! You're being listened to on port " + process.env.PORT );
+    res.write(./index.html);
+    res.end();
+//    res.end( "Congrats! You're being listened to on port " + process.env.PORT );
 });
 
 server.listen( process.env.PORT || 5000 );
